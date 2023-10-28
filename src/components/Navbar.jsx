2be920a8d01bc51203463539f531224579956723
VarunSharma3250 <sv279508@gmail.com>
2023-10-28 13:05:19 -0700

    bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div className='Navabar'>
-            <nav class="bg-color-black navbar navbar-expand-lg navbar-dark bg-dark">
-                <div class="Navbar-opacity container-fluid">
+            <nav ClassName="bg-color-black navbar navbar-expand-lg navbar-dark bg-dark">
+                <div ClassName="Navbar-opacity container-fluid">
                     <Link className="navbar-brand ms-4" to="/">
                         <img
                             src="https://avatars.githubusercontent.com/u/110775856?v=4"
@@ -15,12 +15,12 @@ const Navbar = () => {
                             className="rounded-circle d-inline-block align-text-top me-2 mt-1"
                         />VS
                     </Link>
-                    <span class="navbar-text">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                    <span ClassName="navbar-text">
+                        <button ClassName="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span ClassName="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarText">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <div ClassName="collapse navbar-collapse" id="navbarText">
+                            <ul ClassName="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
                                     <Link className="nav-link active me-4" aria-current="page" to="/Skill#">
                                         <b>Skills</b>
