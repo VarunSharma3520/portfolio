1e95b9fb44de0d33d471478ba199fa18a4dae766
VarunSharma3250 <sv279508@gmail.com>
2023-10-28 13:00:36 -0700

    projects web page completed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,11 +9,29 @@ const project = () => {
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-heading">To</h5>
-                <p class="card-text"><b>Description : </b><br />A project management to-do list is a collection of tasks that outlines the work a project manager or team plans to complete during a project. This list helps project managers understand the various tasks of a project so they can assign work to team members and fulfill their own duties.</p>
-                <a href="https://github.com/VarunSharma3520/GoogleClone" class="mx-2 btn btn-light"><svg aria-hidden="true" height="24" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" class="octicon octicon-mark-github">
-    <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-</svg> Go to GitHuB</a>
+                <h5 class="card-heading">Weather App</h5>
+                <p class="card-text"><b className='card-heading'>Description:</b><br />
+                  Developed a dynamic and informative Weather App using HTML, CSS, JavaScript, Bootstrap for responsive design, and integration with a weather API from RapidAPI to provide users with real-time weather forecasts and data. This project demonstrates proficiency in front-end web development, data integration, and utilization of external APIs.
+                  <br /><a href="https://web3google.netlify.app/" class="m-3 btn btn-success">Go to Project</a>
+                  <a href="https://github.com/VarunSharma3520/GoogleClone" class="mx-2 btn btn-light"><svg aria-hidden="true" height="24" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" class="octicon octicon-mark-github">
+                    <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+                  </svg> Go to GitHuB</a>
+                  <br /><br />
+                  <b className='card-heading'>Key Achievements:</b>
+                  <br />
+                  <b className='card-heading'>Real-Time Weather Data:</b> Integrated with a weather API (e.g., from RapidAPI) to fetch real-time weather information, including temperature, humidity, wind speed, and conditions, allowing users to stay updated with accurate weather forecasts.
+                  <br />
+                  
+                  <b className='card-heading'>Detailed Weather Display:</b> Provided detailed weather information, including current conditions, hourly and daily forecasts, and extended weather data, ensuring comprehensive weather insights.
+                  <br />
+                  <b className='card-heading'>Dynamic Weather Icons:</b> Utilized weather icons to visually represent different weather conditions, enhancing the user experience and making it easy to interpret weather data at a glance.
+                  <br />
+                  <b className='card-heading'>Responsive Design:</b> Leveraged Bootstrap to ensure the app's responsiveness on various devices and various screen sizes, allowing users to access weather data from all over the world to their desktop and mobile platforms.
+                  <br />
+                  <b className='card-heading'>Customization Options:</b> Enabled users to switch between temperature units (e.g., Celsius and Fahrenheit) and customize their preferred view of weather data.
+                  <br />
+                  <b className='card-heading'>Data Storage:</b> Utilized local storage to save user preferences, reducing the need for repeated location detection and improving the user experience.(i.e. it works when broswer allows it).
+                  <br /></p>
               </div>
             </div>
           </div>
@@ -26,7 +44,11 @@ const project = () => {
 
                   Developed a Google Clone web application using React, JavaScript, and CSS to replicate the core functionalities and user interface of the Google search engine. This project served as a hands-on exploration of modern web development techniques and allowed for the practical application of front-end technologies.
                   <br />
-                  <br /><b className='card-heading'>Key Achievements:</b><br /><br /><b className='card-heading'>User Interface Design:</b>Designed and implemented a responsive and user-friendly UI that closely mimics the Google search interface, ensuring a seamless user experience across various devices.
+                  <a href="https://web3google.netlify.app/" class="m-3 btn btn-success">Go to Project</a>
+                <a href="https://github.com/VarunSharma3520/GoogleClone" class="mx-2 btn btn-light"><svg aria-hidden="true" height="24" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" class="octicon octicon-mark-github">
+                  <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+                </svg> Go to GitHuB</a>
+                  <br /><br /><b className='card-heading'>Key Achievements:</b><br /><b className='card-heading'>User Interface Design:</b>Designed and implemented a responsive and user-friendly UI that closely mimics the Google search interface, ensuring a seamless user experience.
                   <br />
                   <b className='card-heading'>Search Functionality:</b> Integrated real-time search functionality that enables users to perform Google-like searches, including predictive text suggestions and dynamic search results.
                   <br />
@@ -36,13 +58,9 @@ const project = () => {
 
                   <br /><b className='card-heading'>State Management:</b> Utilized React's state management to efficiently handle user interactions, maintain search history, and dynamically update the UI.
 
-                  <br /><b className='card-heading'>Deployment:</b> Deployed the application on hosting platforms like Netlify or Vercel, showcasing proficiency in deploying React projects for production use.
+                  <br /><b className='card-heading'>Deployment:</b> Deployed the application Netlify showcasing proficiency in deploying React projects for production use.
 
                   <br /><b className='card-heading'>Responsive Design:</b> Ensured the website's responsiveness, making it accessible and visually appealing on a variety of screen sizes and devices.</p>
-                <a href="https://web3google.netlify.app/" class="mx-2 btn btn-success">Go to Project</a>
-                <a href="https://github.com/VarunSharma3520/GoogleClone" class="mx-2 btn btn-light"><svg aria-hidden="true" height="24" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" class="octicon octicon-mark-github">
-    <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-</svg> Go to GitHuB</a>
               </div>
             </div>
           </div>
